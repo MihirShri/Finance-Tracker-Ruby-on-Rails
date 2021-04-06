45779e8a14bdb20556211717325f5c8ed24a476f
Mihir Shri <mihir.shri10@gmail.com>
2021-04-06 19:55:51 +0530

    Add first_name and last_name to sign_up and edit_profile

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,10 +7,14 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key], endpoint: 'https://sandbox.iexapis.com/v1')
     begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))     # Equivalent to Stock.new
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 
 end
