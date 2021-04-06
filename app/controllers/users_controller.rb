f3c6cc18b73d6b6a22ac3c6edf78d1ae1d918c8e
Mihir Shri <mihir.shri10@gmail.com>
2021-04-06 21:20:01 +0530

    Add friends listing view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def my_friends
+    @tracked_friends = current_user.friends
+  end
 end
