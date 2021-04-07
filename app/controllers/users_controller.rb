e6af3b525102e65ee5a95a56e6bdcde3b45fa4ae
Mihir Shri <mihir.shri10@gmail.com>
2021-04-07 15:44:30 +0530

    Ready for Production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
@@ -28,4 +30,10 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show], :constraints => { :id => /[0-9|]+/ }
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
